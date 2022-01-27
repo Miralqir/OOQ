34445937756a3e306aa60a8e53bbf95de82267ff
VintilaMMM <vintilammm@gmail.com>
2022-01-27 22:41:51 +0200

    Revert "Sixth edition"
    This reverts commit f9e6283c91161fdc7aea327d6c0e8fc8474122f6.
    
    	deleted:    data/ui/background_quiz.png
    	deleted:    data/ui/crem_background.png
    	deleted:    data/ui/new_continue.png
    	modified:   data/ui/new_exit.png
    	deleted:    data/ui/selected_continue_button1.png
    	deleted:    data/ui/selected_exit_button.png
    	deleted:    data/ui/sign.png
    	modified:   data/ui/text_box.png
    	deleted:    data/ui/text_box_2.png
    	deleted:    data/ui/txt_box.png
    	deleted:    data/ui/txt_box_2.png
    	modified:   include/ui.h
    	modified:   include/utilities.h
    	modified:   src/ui.cpp
    	modified:   src/utilities.cpp

--- include/ui.h
+++ include/ui.h
@@ -5,12 +5,6 @@
 #include "game.h"
 
 #include <list>
-#include <vector>
-#include <algorithm>
-#include <sstream>
-#include <string>
-#include <iterator>
-#include <cmath>
 
 class Manager;
 class GameManager;
@@ -25,34 +19,11 @@ private:
 	GameManager *game_manager;
 
 	bool paused;
-	bool in_quiz;
 
-	std::list<Texture>::iterator background;
-	std::list<Texture>::iterator background_quiz;
-	std::list<Texture>::iterator exit_button;
-	std::list<Texture>::iterator continue_button;
-	std::list<Texture>::iterator selected_exit_button;
-	std::list<Texture>::iterator selected_continue_button;
-	std::list<Texture>::iterator time_edit1;
-	std::list<Texture>::iterator time_edit2;
-	std::list<Texture>::iterator text_box_short1;
-	std::list<Texture>::iterator text_box_short2;
-	std::list<Texture>::iterator text_box_long;
-	std::list<Texture>::iterator text_box;
-	std::list<Texture>::iterator sign;
-
-	std::vector<std::list<Texture>::iterator> letters;
-	std::vector<std::list<Texture>::iterator> digits_black;
-	std::vector<std::list<Texture>::iterator> digits_white;
-	std::vector<std::list<Texture>::iterator> digits_green;
+	std::list<Texture>::iterator logo;
 
 public:
 	UIManager(Manager *parent);
 
 	void runTick(uint64_t delta);
-	void renderText(std::string text, int pos_x, int pos_y, int max_x);
-	void renderNumber(std::vector<std::list<Texture>::iterator> type_digits, int number, int pos_x, int pos_y);
-	void renderTimePart(uint64_t time, int pos_x, int pos_y, int* final_pos);
-	void renderTime(uint64_t time, int pos_x, int pos_y);
-	void displayQuiz(std::string question, std::vector<std::string> answers);
 };
--- include/utilities.h
+++ include/utilities.h
@@ -7,12 +7,6 @@
  * note: consider using templates?
  */
 
-#include <stdint.h>
-
 enum DIR {UP = 0, LEFT, DOWN, RIGHT, DIR_SIZE};
 
 int sgn(int nr);
-
-int countDigit(int n);
-
-void getTime(uint64_t time, uint64_t* hours, uint64_t* minutes, uint64_t* seconds);
